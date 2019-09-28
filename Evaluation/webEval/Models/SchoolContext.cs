9a489af82fcd08dfaae41acbe8f9d7caa48ac8cb
BuildTools <unconfigured@null.spigotmc.org>
2019-09-28 01:56:50 -0400

    Primer commit

---
+++ Evaluation/webEval/Models/SchoolContext.cs
@@ -0,0 +1,13 @@
+﻿namespace mvcAddress.Models
+{
+    using System.Data.Entity;
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<webEval.Models.Student> Students { get; set; }
+    }
+}
